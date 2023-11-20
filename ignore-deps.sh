5cb07eb3e9113ab57c2f650d7de89ec2f16343ce
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-11-20 19:59:57 +0800

    fix(tsc): nocheck rather than ignore
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- ignore-deps.sh
+++ ignore-deps.sh
@@ -3,5 +3,5 @@
 # Set the directory path
 directory_path="node_modules"
 
-# Find TypeScript files in the specified directory and add '@ts-ignore' at the top
-find "$directory_path" -type f -name '*.ts' -exec sed -i '1i\// @ts-ignore' {} \;
+# Find TypeScript files in the specified directory and add '@ts-nocheck' at the top
+find "$directory_path" -type f -name '*.ts' -exec sed -i '1i\// @ts-nocheck' {} \;
