1f03e2b9d626fc4dfad186b55e51ba5a9dc57bbe
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-11-27 16:24:45 +0800

    feat(disconnect): use new check / property
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -5,11 +5,11 @@ export const command: Command = {
     name: 'disconnect',
     description: 'Stops the player and disconnects Celerity.',
     aliases: [ 'dc', 'fuckoff', 'leave' ],
-    checks: [ 'vc', 'dj', 'player' ],
+    checks: [ 'vc', 'dj', 'player', 'connection' ],
     options: [],
 
-    async execute({ client, context, player }) {
-        const voiceChannel = context.guild!.channels.cache.get(player.player.connection.channelId!)!;
+    async execute({ client, context, player, connection }) {
+        const voiceChannel = context.guild!.channels.cache.get(connection!.channelId!)!;
         if (voiceChannel.type !== ChannelType.GuildVoice && voiceChannel.type !== ChannelType.GuildStageVoice) {
             client.logger.error(`Disconnected from ${ voiceChannel.name } (${ voiceChannel.id }) in ${ context.guild!.name } (${ context.guild!.id }) due to an unknown channel type: ${ voiceChannel.type }`);
             return client.respond(context.channel, `${ client.config.emojis.error } | **Disconnected due to an internal error.**`, 'error');
@@ -17,8 +17,8 @@ export const command: Command = {
         const members = voiceChannel.members.filter((m: GuildMember) => !m.user.bot && !m.voice.deaf);
         if (members.size > 0 && player.current && context.member!.voice.channelId !== context.guild!.members.me!.voice.channelId)
             return client.respond(context.channel, `${ client.config.emojis.error } | **You're not in <#${ context.guild!.members.me!.voice.channel!.id }>.**`, 'error');
-        client.respond(context.channel, `${ client.config.emojis.disconnect } | **Disconnected from <#${ player.player.connection.channelId }>.**`, 'success');
-        player.player.connection.disconnect();
+        client.respond(context.channel, `${ client.config.emojis.disconnect } | **Disconnected from <#${ connection!.channelId }>.**`, 'success');
+        connection!.disconnect();
         player.destroy();
         return;
     }
