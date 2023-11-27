eaa676cffc928ab5e8698936c3af98d6368a9efe
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-11-27 16:34:50 +0800

    fix(events): fix connection success message
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/events/shoukakuReady.ts
+++ src/events/shoukakuReady.ts
@@ -6,6 +6,6 @@ export const event: Event = {
     emitter: 'shoukaku',
 
     async run(client) {
-        client.logger.info(`Connected to Lavalink - ${ client.config.lavalinkSecure.toLowerCase() === 'true' ? 'wss://' : 'ws://' }${ client.config.lavalinkUrl }`);
+        client.logger.info(`Connected to Lavalink - ${ client.config.lavalink.secure ? 'wss://' : 'ws://' }${ client.config.lavalink.host }:${ client.config.lavalink.port }`); 
     }
 };
