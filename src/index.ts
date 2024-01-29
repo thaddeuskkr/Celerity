e6d9d6be9feb7f4e1f4e11ece47022b782e99546
Thaddeus Kuah <tk@tkkr.dev>
2024-01-29 17:38:59 +0000

    chore(license): new year :) [skip ci]
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /*
     Celerity, a music Discord bot focused on performance, without sacrificing functionality, features and quality.
-    Copyright (C) 2023 • Thaddeus Kuah • thaddeuskkr
+    Copyright (C) 2024 • Thaddeus Kuah • thaddeuskkr
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
