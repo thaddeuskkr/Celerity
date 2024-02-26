e55494fa3194436a26ebaa4d64c6d1c0e959786b
Thaddeus Kuah <tk@tkkr.dev>
2024-02-26 08:30:15 +0000

    fix(avatar): reduce lines of code, catch potential error
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/commands/util/avatar.ts
+++ src/commands/util/avatar.ts
@@ -18,14 +18,13 @@ export const command: Command = {
     async execute({ client, context, args, settings }) {
         let user = context.author;
         if (context.mentions.users.size > 0) user = context.mentions.users.first()!;
-        else if (args.length) {
-            if (!args[0]) return client.respond(context.channel, `${client.config.emojis.error} | **User not provided.**`, 'error');
+        else if (args.length && args[0]) {
             await context.guild?.members.fetch(); // I can see this becoming a huge memory issue in the future, but we'll have to see.
             const fetchedUser =
                 context.guild!.members.cache.find((m) => m.user.username.toLowerCase() === args.join(' ').toLowerCase())?.user ||
                 context.guild!.members.cache.find((m) => m.user.globalName?.toLowerCase() === args.join(' ').toLowerCase())?.user ||
                 context.guild!.members.cache.find((m) => m.nickname?.toLowerCase() === args.join(' ').toLowerCase())?.user ||
-                (await client.users.fetch(args[0]));
+                (await client.users.fetch(args[0]).catch(() => null));
             if (fetchedUser) user = fetchedUser;
             else return client.respond(context.channel, `${client.config.emojis.error} | **Invalid user.**`, 'error');
         }
