5223a6eccf4346754b8092cfaba9ead7f789a528
Thaddeus Kuah <tk@tkkr.dev>
2024-02-26 11:45:33 +0000

    style(prettier): run prettier
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/commands/owner/players.ts
+++ src/commands/owner/players.ts
@@ -16,7 +16,7 @@ export const command: Command = {
             players.forEach((player) => {
                 const guild = player.guild;
                 playerList.push(
-                    `- **${guild.name} (\`${guild.id}\`)** | ${player.queue.length} (\`${player.ms(player.queue.totalDuration)}\`) tracks in queue | ${player.stopped ? 'Stopped' : player.player.paused ? 'Paused' : 'Playing'}${player.current ? ` [**${player.current.info.title}** by **${player.current.info.author}**](${player.current.info.uri}) \`${player.ms(player.current.info.length)}\`` : ''}`
+                    `- **${guild.name} (\`${guild.id}\`)** | ${player.queue.length} (\`${player.ms(player.queue.totalDuration)}\`) tracks in queue | ${player.stopped ? 'Stopped' : player.player.paused ? 'Paused' : 'Playing'}${player.current ? ` [**${player.current.info.title}** by **${player.current.info.author}**](${player.current.info.uri}) \`${player.ms(player.current.info.length)}\`` : ''}`,
                 );
             });
             return context.reply(`# Active players\n${playerList.join('\n')}`);
