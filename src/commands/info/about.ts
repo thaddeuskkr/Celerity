d9d1448a16089a0881df6bb977059d91f1adcae7
Thaddeus Kuah <tk@tkkr.dev>
2024-02-26 17:16:34 +0000

    feat(about): add user and server count
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/commands/info/about.ts
+++ src/commands/info/about.ts
@@ -12,6 +12,8 @@ export const command: Command = {
 
     async execute({ client, context, settings }) {
         if (!client.user) return;
+        let userCount = 0;
+        client.guilds.cache.forEach((guild) => (userCount += guild.memberCount));
         client.respond(
             context,
             new EmbedBuilder()
@@ -25,6 +27,8 @@ export const command: Command = {
                         Celerity is programmed in [TypeScript](https://www.typescriptlang.org/) using [Node.js®](https://nodejs.org) - and utilises [discord.js](https://discord.js.org) v${client.dependencies['discord.js']?.replace('^', '')} and [Lavalink](https://lavalink.dev). Celerity was created on <t:1682870400:D> (<t:1682870400:R>) and is currently on version \`${client.version}\`. This instance of Celerity has been running for ${ms(process.uptime() * 1000, { verbose: true, secondsDecimalDigits: 0, millisecondsDecimalDigits: 0 })}. 
                         
                         Celerity receives constant updates and improvements, and its developer is always open to suggestions and feedback. Please do note that Celerity is developed in its developer's free time, so updates may not be as frequent as one might expect.
+
+                        Celerity is in **${client.guilds.cache.size} servers** and serves **${userCount} users**.
 
                         **To contact the developer, please drop an email to \`tk@tkkr.dev\` or send a text to \`@t.kkr\` on Discord.**
 
