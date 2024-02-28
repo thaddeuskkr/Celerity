209d0db5ea4c405a05705a737aee0e0ceef1735c
Thaddeus Kuah <tk@tkkr.dev>
2024-02-28 15:01:50 +0000

    feat(announce): add an announcement command
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

---
+++ src/commands/owner/announce.ts
@@ -0,0 +1,40 @@
+import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import type { Command } from '../../types';
+
+export const command: Command = {
+    name: 'announce',
+    description: 'Sends an announcement to all active players.',
+    aliases: [],
+    checks: ['owner'],
+    userPermissions: [],
+    options: [
+        {
+            name: 'text',
+            description: 'The text to send to each server.',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        },
+    ],
+
+    async execute({ client, context, args }) {
+        const players = client.players;
+        const announcement = args.join(' ');
+        if (!players.size) return client.respond(context, `${client.config.emojis.error} | **No active players.**`, 'error');
+        else {
+            const playerList: Array<string> = [];
+            players.forEach((player) => {
+                playerList.push(
+                    `- **${player.guild.name} (\`${player.guild.id}\`)**`,
+                );
+                player.channel.send({ embeds: [
+                    new EmbedBuilder()
+                        .setAuthor({ name: `@${context.author.username} - Announcement`, iconURL: context.author.displayAvatarURL({ size: 4096 }) })
+                        .setDescription(announcement)
+                        .setTimestamp()
+                        .setColor('#fab387')
+                ] });
+            });
+            await context.reply({ content: `## Announcement sent\n${playerList.join('\n')}`, allowedMentions: { repliedUser: false } });
+        }
+    },
+};
