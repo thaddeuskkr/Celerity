3eff935c1c816310f06202a6a22665654ff26819
Thaddeus Kuah <tk@tkkr.dev>
2024-02-28 14:55:41 +0000

    fix(players): simplify code
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/commands/owner/players.ts
+++ src/commands/owner/players.ts
@@ -14,9 +14,8 @@ export const command: Command = {
         else {
             const playerList: Array<string> = [];
             players.forEach((player) => {
-                const guild = player.guild;
                 playerList.push(
-                    `- **${guild.name} (\`${guild.id}\`)** | ${player.queue.length} tracks in queue (\`${player.ms(player.queue.totalDuration)}\`) | ${player.stopped ? 'Stopped' : player.player.paused ? 'Paused' : 'Playing'}${player.current ? ` [**${player.current.info.title}** by **${player.current.info.author}**](<${player.current.info.uri}>) \`${player.ms(player.position)}\`/\`${player.ms(player.current.info.length)}\`` : ''}`,
+                    `- **${player.guild.name} (\`${player.guild.id}\`)** | ${player.queue.length} tracks in queue (\`${player.ms(player.queue.totalDuration)}\`) | ${player.stopped ? 'Stopped' : player.player.paused ? 'Paused' : 'Playing'}${player.current ? ` [**${player.current.info.title}** by **${player.current.info.author}**](<${player.current.info.uri}>) \`${player.ms(player.position)}\`/\`${player.ms(player.current.info.length)}\`` : ''}`,
                 );
             });
             return context.reply({ content: `## Active players\n${playerList.join('\n')}`, allowedMentions: { repliedUser: false } });
