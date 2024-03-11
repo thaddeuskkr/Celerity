2fe67954b44b9cd1fa54bf99ea3ce5ca68771950
Thaddeus Kuah <thaddeuskkr@gmail.com>
2024-03-11 23:31:59 +0800

    chore: remove extra text [skip ci]
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/info/about.ts
+++ src/commands/info/about.ts
@@ -26,13 +26,13 @@ export const command: Command = {
                 .setURL('https://celerity.tkkr.dev')
                 .setDescription(
                     tags.stripIndents`
-                        *Celerity is a free advanced music Discord bot created with love by [\`@t.kkr\`](https://tkkr.dev) that promises to be fast, performant and feature-rich, without neglecting audio quality or user-friendliness.*
+                        *Celerity is a free advanced music Discord bot created by [\`@t.kkr\`](https://tkkr.dev) that promises to be fast, performant and feature-rich, without neglecting audio quality or user-friendliness.*
 
                         Celerity is programmed in [TypeScript](https://www.typescriptlang.org/) and is currently running on [Node.js®](https://nodejs.org) ${process.version}. Celerity utilises [discord.js](https://discord.js.org) v${require('discord.js').version}, along with [Lavalink](https://lavalink.dev) to play high-quality audio without sacrificing performance.
                         
                         Celerity was created on <t:1682870400:D> (<t:1682870400:R>) and is currently on version \`${client.version}\`, with ${lines['SUM']['code']} total lines of code in its codebase (not including libraries). This instance of Celerity has been running for ${ms(process.uptime() * 1000, { verbose: true, secondsDecimalDigits: 0, millisecondsDecimalDigits: 0 })}.
                         
-                        Celerity receives constant updates and improvements, and its developer is always open to suggestions and feedback. Please do note that Celerity is developed in its developer's free time, so updates may not be as frequent as one might expect.
+                        Celerity receives constant updates and improvements, and its developer is always open to suggestions and feedback.
 
                         Celerity is in **${client.guilds.cache.size} servers** and serves **${userCount} users**.
 
